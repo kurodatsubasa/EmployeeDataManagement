da9fee438dbdc741afec3bed8bc30c098ee64c13
brshroda <35981932+brshroda@users.noreply.github.com>
2018-02-02 18:14:43 -0600

    Implemented employeeid

--- Supervisor.java
+++ Supervisor.java
@@ -34,6 +34,10 @@ public class Supervisor extends SalEmployee {
         this.aBonus = aBonus;
     } // end setABonus
     
+    public void getEmployeeId() {
+        
+    }
+    
      // overriding abstract method to calculate pay for supervisor, 
     @Override
     public double getGrossWeeklyPay() {
